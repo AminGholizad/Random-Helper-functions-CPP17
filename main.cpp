3742c08b15d418d2529d6639385af012547aed97
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-06-01 02:33:16 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
   double x = rnd::rand(); //uniform random double between 0-1
   int y = rnd::unifrnd(1,5); //uniform random int between 1-5
-  float z = rnd::unifrnd(0.1f,0.5f); //uniform random float between 0.1-0.5
+  float z = rnd::unifrnd<float>(0.1,0.5); //uniform random float between 0.1-0.5
   std::vector<std::string> v{"Hello!","Hi!","How are you?","Good bye!"};
   std::string s = *rnd::select_randomly(v.begin(),v.end()); // select a random entry from the vector
   std::cout << "x=" << x << '\n';
