f67fad27bba5d8b8664181c5b2a2f4341c2709a9
Amin Gholizad <amin.gholizad@gmail.com>
2020-12-12 04:16:08 +0330

    Uniform initialization & sing convertion warninig

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include "rand.hpp"
 #include <vector>
 int main() {
-  double x = rnd::rand(); //uniform random double between 0-1
-  int y = rnd::unifrnd(1,5); //uniform random int between 1-5
-  float z = rnd::unifrnd<float>(0.1,0.5); //uniform random float between 0.1-0.5
+  double x{rnd::rand()}; //uniform random double between 0-1
+  int y{rnd::unifrnd(1,5)}; //uniform random int between 1-5
+  float z{rnd::unifrnd<float>(0.1,0.5)}; //uniform random float between 0.1-0.5
   std::vector<std::string> v{"Hello!","Hi!","How are you?","Good bye!"};
-  std::string s = *rnd::select_randomly(v.begin(),v.end()); // select a random entry from the vector
+  std::string s{*rnd::select_randomly(v.begin(),v.end())}; // select a random entry from the vector
   std::cout << "x=" << x << '\n';
   std::cout << "y=" << y << '\n';
   std::cout << "z=" << z << '\n';
